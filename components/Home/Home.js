ee5bbdfdaad4527f3b0407888dec3a8743771008
saphalpantha <saphalpantha@gmail.com>
2023-02-17 22:15:14 +0530

    responsive content 60%

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 import { Main } from '../main/Main'
 import SideBar from '../sidebar/SideBar'
 import classes from './Home.module.css';
+import styles from '../../styles/Home.module.css'
 const Container = (props) => {
   // const [isOpen , setIsOpen] = useState(true);
   // const sidebarCloseHandler = () => {
   //   setIsOpen((toggle) => !toggle)
   // }
 
+
+
   return (
-    <div className={classes.home}>
+    <div className={`${classes.home}` }>
       {/* <h1 onClick={sidebarCloseHandler} className= {`${classes.toggle} ${isOpen  ? classes.open: classes.close}`}>{isOpen ? 'Close' : 'Open'}</h1> */}
         {/* {isOpen && <SideBar/>} */}
         <SideBar/>
