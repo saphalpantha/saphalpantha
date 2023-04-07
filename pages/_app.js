418e1a005dbd87e575f7dd3879e45e43548018b0
saphalpantha <saphalpantha@gmail.com>
2023-04-07 10:14:07 +0530

    Added Edu & Exp

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,19 @@ import { Fragment } from 'react'
 import '../styles/globals.css'
 import Head from 'next/head'
 import SideBar from '../components/sidebar/SideBar'
+import Script from 'next/script'
 // import icon from '../public/images/icon.ico'
 export default function App({ Component, pageProps }) {
   return <Fragment>
+          <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8789008287651331"
+     crossorigin="anonymous"></Script>
           <Head>
             <meta name="viewport" content="initial-scale=1.0 width=device-width"/>
             <link rel="icon" href="/images/icon.ico" type="image/x-icon"></link>
         <title>Saphal Pantha</title>
         <meta name="description" content="Functional Portfolio page of author saphal pantha"></meta>
     </Head>
+    
    <Component {...pageProps} />
   </Fragment>
 }
