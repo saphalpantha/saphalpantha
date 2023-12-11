a8ffcd79bd397940305b94e36db46eb22085b526
saphalpantha <saphalpantha@gmail.com>
2023-12-11 23:22:25 +0545

    docker_dep14

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -6,7 +6,7 @@ import MapView from './MapView'
 import {motion as m } from 'framer-motion'
 const Contact = () => {
   return (
-    <m.div  className={classes.contactflex} initial={{opacity:0}} animate={{opacity:1} } transition={{duration:0.85,ease:'easeOut'}}>
+    <m.div  className={classes.contactflex} initial={{opacity:0}} animate={{opacity:1} } transition={{duration:0.85,ease:'easeIn'}}>
         <header className={classes.heading}>
             <span className={classes.hiii}>
                 <h3>Contact</h3>
